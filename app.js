3b96533a688a720bb2569b327e24e21c3a12da81
Amit korgavkar <korgavkaramitkrushna1031@gmail.com>
2026-06-06 18:12:25 +0530

    add new listing route and add boostrap and custome styling

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const Listing = require("./Models/listings.js");
 const path = require("path");
 require("dotenv").config();
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")))
 
 main().then((res) =>{
     console.log("Connection Successful");
@@ -42,10 +45,10 @@ app.get("/listings/:id", async(req, res) =>{
 })
 
 //Create Route
-app.post("/listings", (req, res) =>{
+app.post("/listings", async (req, res) =>{
     let listing = req.body.listing;
-    console.log(listing)
-    res.send("Success")
+    await Listing.insertOne(listing);
+    res.redirect("/listings")
 })
 
 //Edit Route
