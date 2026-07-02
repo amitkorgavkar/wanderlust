45b80985e26f7e2fe1ca8edf7c66e9dbede6a106
Amit korgavkar <korgavkaramitkrushna1031@gmail.com>
2026-07-02 10:16:49 +0530

    feat: implement user login and authentication

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/user.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) =>{
     res.render("users/signup");
 })
 
 router.post("/signup", async(req, res) =>{
-    let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    let registeredUser = await User.register(newUser, password)
-    // console.log(registeredUser);
+    try{
+        let {username, email, password} = req.body;
+        const newUser = new User({email, username});
+        let registeredUser = await User.register(newUser, password);
+        req.flash("success", "Welcome to WanderLust! You are logged in!");
+        res.redirect("/listings")
+    }catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup")
+    }
+});
+
+router.get("/login", (req, res) =>{
+    res.render("users/login");
+})
+
+router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), async(req, res) =>{
     req.flash("success", "Welcome to WanderLust!");
-    res.redirect("/listings")
+    res.redirect("/listings");
 })
 
 module.exports = router;
