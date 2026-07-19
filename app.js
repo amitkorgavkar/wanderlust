75111e49747a5b280436ce4c4ce923f8081753f4
Amit korgavkar <korgavkaramitkrushna1031@gmail.com>
2026-07-19 15:15:08 +0530

    Fix Home Route

--- app.js
+++ app.js
@@ -76,6 +76,10 @@ app.use((req, res, next) =>{
     next();
 });
 
+app.get("/", (req, res) =>{
+    res.redirect("/listings")
+})
+
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/listings", listingRouter);
 app.use("/", userRouter);
